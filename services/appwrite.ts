5b175f8730e2fb8f65c8f47e3758ca01385c8f15
mygo <1158270624@qq.com>
2025-12-27 20:48:16 +0800

    ✨ feat:：添加Appwrite搜索统计服务

---
+++ services/appwrite.ts
@@ -0,0 +1,53 @@
+// 记录用户的搜索记录
+import { Client, Databases, ID, Query } from "react-native-appwrite";
+
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
+
+const client = new Client()
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (query: string, movie: Movie) => {
+  try {
+    const result = await database.listDocuments(
+      DATABASE_ID,
+      COLLECTION_ID,
+      [Query.equal("searchTerm", query)]
+    );
+    
+    // 检查该搜索记录是否已存在
+    if (result.documents.length > 0) {
+      // 若存在，则增加搜索次数
+      const existingMovie = result.documents[0];
+      await database.updateDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        existingMovie.$id,
+        {
+          count: (existingMovie.count || 0) + 1,
+        }
+      );
+    } else {
+      // 若不存在，则在 Appwrite 中新建记录
+      await database.createDocument(
+        DATABASE_ID, 
+        COLLECTION_ID, 
+        ID.unique(), 
+        {
+          searchTerm: query,
+          movie_id: movie.id,
+          title: movie.title,
+          count: 1,
+          poster_url: movie.poster_path 
+            ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` 
+            : null,
+        }
+      );
+    }
+  } catch (err) {
+    console.error("记录搜索失败:", err);
+  }
+}
