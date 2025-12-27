d49dcafa24c2b908b3d87ac61b5c340a1cd0ee28
mygo <1158270624@qq.com>
2025-12-27 21:41:26 +0800

    ✨feat: 新增getTrendingMovies函数，用于获取搜索量前5的电影数据

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -51,3 +51,19 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
     console.error("记录搜索失败:", err);
   }
 }
+
+export const getTrendingMovies = async (): Promise<
+  TrendingMovie[] | undefined
+> => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.limit(5),
+      Query.orderDesc("count"),
+    ]);
+
+    return result.documents as unknown as TrendingMovie[];
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
+};
